6b9bc647f5b80eb40b7997ae69d8d428712fa2ce
happy-coding-0036 <emiko0036@hotmail.com>
2021-06-06 05:27:52 -0400

    changed menu

--- src/menu.js
+++ src/menu.js
@@ -8,7 +8,7 @@ const menuContent = function() {
    const container = buildElement("div", "containerStyle", null, "containerMenuId")
 
   const heading = buildElement("h1", "info3", "menu heading", "menuID")
-  const para =  buildElement("p", "info4", "menu menu menu here is a long sentence or paragraph that I don't know how to add or how to remove.", "paraMenuID")
+  const para =  buildElement("p", "info4", "this is a test. menu menu menu here is a long sentence or paragraph that I don't know how to add or how to remove.", "paraMenuID")
 
   const content = document.getElementById("content")
  const headContent =  container.appendChild(heading)
